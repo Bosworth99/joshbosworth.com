3b3da28e5e0e0f298315373a16aca4b98fc85b74
Josh <josh@joshbosworth.com>
2016-01-20 10:32:15 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <!--<![endif]-->
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+
+		<!-- TITLE -->
+		<title>JoshBosworth.com | Josh Bosworth - Olympia, Wa</title>
+
+		<!-- SEO -->
+		<meta name="description" content="I'm a web developer and designer.">
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- CSS -->
+		<link id="css-base" rel="stylesheet" href="css/base.css"/>
+		<link id="css-layout" rel="stylesheet" href="css/layout.css"/>
+
+		<!-- HEAD SCRIPTS -->
+		<script type="text/javascript" src="../js/libs/modernizr/modernizr.js"></script>
+
+	</head>
+	<body>
+
+		<!-- begin body -->
+		<div class="body-wrapper">
+
+			<!-- begin header -->
+			<header class="header scaffold">
+				<div class="main-wrapper container">
+					<div class="row">
+						<div class="main">
+							<h1>joshbosworth.com</h1>
+						</div>
+					</div>
+				</div>
+			</header>
+			<!-- end header -->
+
+			<!-- begin content -->
+			<section class="content scaffold">
+				<div class="alpha container">
+					<div class="row">
+						<section class="link-wrapper">
+							<ul class="links">
+								<li class="link email">
+									<span class="icon"></span>
+									<a href="mailto:josh@joshbosworth.com" target="_blank">
+										<h3>josh@joshbosworth.com</h3>
+									</a>
+								</li>
+								<li class="link facebook">
+									<span class="icon"></span>
+									<a href="http://www.facebook.com/josh.bosworth" target="_blank">
+										<h3>facebook</h3>
+									</a>
+								</li>
+								<li class="link google">
+									<span class="icon"></span>
+									<a href="https://plus.google.com/107072385947308037500/posts" target="_blank">
+										<h3>google+</h3>
+									</a>
+								</li>
+								<li class="link stackoverflow">
+									<span class="icon"></span>
+									<a href="http://stackoverflow.com/users/653929/bosworth99" target="_blank">
+										<h3>stackoverflow</h3>
+									</a>
+								</li>
+								<li class="link instagram">
+									<span class="icon"></span>
+									<a href="http://ink361.com/#/users/33299653/photos" target="_blank">
+										<h3>instagr.am</h3>
+									</a>
+								</li>
+								<li class="link location">
+									<span class="icon"></span>
+									<a href="http://goo.gl/maps/0rBs" target="_blank">
+										<h3>Olympia, Wa</h3>
+									</a>
+								</li>
+							</ul>
+						</section>
+					</div>
+				</div>
+			</section>
+			<!-- end content -->
+
+			<!-- begin footer -->
+			<footer class="footer scaffold">
+				<div class="main-wrapper container">
+					<div class="row">
+						<section class="main">
+						</section>
+					</div>
+				</div>
+				<div class="colophon-wrapper container">
+					<div class="row">
+						<section class="colophon">
+						</section>
+					</div>
+				</div>
+			</footer>
+			<!-- end footer -->
+
+		</div>
+		<!-- end body -->
+
+		<!-- begin fixed
+		<div class="fixed">
+
+		</div>
+		<!-- end fixed -->
+
+		<!-- begin scripts -->
+		<script type="text/javascript" src="../js/libs/jquery/jquery.1.7.2.js"></script>
+		<!-- end scripts -->
+
+	</body>
+</html>
